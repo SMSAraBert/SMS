bb316c200c605936d953f6073dfa176d772d712f
SMSAraBert <163198044+SMSAraBert@users.noreply.github.com>
2024-03-17 05:38:39 +0800

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 from flask import render_template
 import json
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 import pickle
 # sms text preprocessiing 
 from spellchecker import SpellChecker
