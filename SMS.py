567a22aa445bfaa9fbcfc33db4b1af238eb9cda7
SMSAraBert <163198044+SMSAraBert@users.noreply.github.com>
2024-03-17 10:13:40 +0800

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -339,7 +339,7 @@ def preprocessing(text):
 
 # Define the API endpoint for receiving SMS messages
 app = Flask(__name__)
-@app.route('/SMS/', methods=['POST'])
+@app.route('/SMS', methods=['POST'])
 def receive_sms():
     input_text = request.form.get('Body')
     if input_text is None:
