0cd26de9738fca4fd3702a0761aa0aab1938d99b
SMSAraBert <163198044+SMSAraBert@users.noreply.github.com>
2024-03-24 03:50:57 +0800

    Update SMSApp.py

--- SMSApp.py
+++ SMSApp.py
@@ -90,4 +90,4 @@ def index():
     return render_template('index.html')
 if __name__ == '__main__':
     # app.run(debug=True)
-     app.run(host='localhost', port=8080, debug=True)
+     app.run(debug=True)
